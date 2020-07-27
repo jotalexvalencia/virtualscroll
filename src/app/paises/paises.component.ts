3f9bf810ecfcfccb7240c14f47378495a4486f35
jorge alexander valencia valencia <jorgealexandervalencia@gmail.com>
2020-07-27 16:15:51 -0500

    virtualscroll y drang and drop en angular ejemplo

---
+++ src/app/paises/paises.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-paises',
+  templateUrl: './paises.component.html',
+  styleUrls: ['./paises.component.css']
+})
+export class PaisesComponent implements OnInit {
+
+  paises: any = [];
+
+  constructor( private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get(`https://restcountries.eu/rest/v2/lang/es`)
+    .subscribe( paises => this.paises = paises);
+  }
+
+  drop(evento: CdkDragDrop<any>) {
+    // console.log('Ok!', evento);
+    // me interesa el previuxIndex(anterior) y el currentIndex(actual)
+    // parametros cual es el arreglo actual,cual es el index anterior,
+    moveItemInArray(this.paises, evento.previousIndex, evento.currentIndex);
+  }
+
+}
